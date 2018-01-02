e6c357f6fd53f195fc4ca0d55a10f1436d75bda4
benyi <benjausen@gmail.com>
2018-01-02 00:26:33 -0300

    Working on tilemapping...

--- map.c
+++ map.c
@@ -1,1 +1,12 @@
 #include "map.h"
+#include "utilities.h"
+
+const int tile_size = 32;
+vec2 map_size.x = 50;
+vec2 map_size.y = 50;
+
+void map_tile_render(tile *tl)
+{
+    //SDL_RenderCopy
+}
+
--- map.h
+++ map.h
@@ -1,6 +1,35 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <SDL.h>
+#include <SDL_image.h>
 
+const int tile_size;
+
+vec2 map_size;
+
+typedef enum
+{
+    TILE_EMPTY,
+    TILE_SOLID,
+    TILE_ONEWAY
+}
+tile_type;
+
+typedef struct
+{
+    vec2 pos;
+    tile_type type;
+    SDL_Texture *texture;
+}
+tile;
+
+typedef struct
+{
+    tile tiles[map_size.x][map_size.y];
+}
+map;
+
+map_tile_render(tile *tl);
 
 #endif
